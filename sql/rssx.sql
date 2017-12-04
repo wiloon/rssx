0adb501db12a85324a48f979d00cb5874b80fd31
wiloon <wiloon.wy@gmail.com>
2017-12-04 13:16:39 +0800

    sql change

--- sql/rssx.sql
+++ sql/rssx.sql
@@ -2,7 +2,7 @@
 -- Host:                         localhost
 -- Server version:               10.1.29-MariaDB - MariaDB Server
 -- Server OS:                    Linux
--- HeidiSQL Version:             9.4.0.5130
+-- HeidiSQL Version:             9.4.0.5174
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,14 +16,21 @@
 CREATE DATABASE IF NOT EXISTS `rssx` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `rssx`;
 
--- Dumping structure for table rssx.feeds
-CREATE TABLE IF NOT EXISTS `feeds` (
-  `feed_id` int(11) DEFAULT NULL,
-  `title` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `url` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Dumping structure for table rssx.feed
+CREATE TABLE IF NOT EXISTS `feed` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table rssx.feed: ~2 rows (approximately)
+/*!40000 ALTER TABLE `feed` DISABLE KEYS */;
+INSERT INTO `feed` (`id`, `title`, `url`) VALUES
+	(1, 'OS China', 'http://www.oschina.net/news/rss'),
+	(2, 'InfoQ', 'http://www.infoq.com/cn/feed');
+/*!40000 ALTER TABLE `feed` ENABLE KEYS */;
+
 -- Dumping structure for table rssx.news
 CREATE TABLE IF NOT EXISTS `news` (
   `news_id` int(11) DEFAULT NULL,
@@ -34,7 +41,6 @@ CREATE TABLE IF NOT EXISTS `news` (
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Data exporting was unselected.
 -- Dumping structure for table rssx.user
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) DEFAULT NULL,
@@ -42,14 +48,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   `create_time` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table rssx.user: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
 -- Dumping structure for table rssx.user_feed
 CREATE TABLE IF NOT EXISTS `user_feed` (
   `user_id` int(11) DEFAULT NULL,
   `feed_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table rssx.user_feed: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user_feed` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_feed` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
