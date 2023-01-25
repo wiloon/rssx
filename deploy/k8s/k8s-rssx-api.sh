87eb022c51063f34cc8a873c64bd265243c15a11
Yue <wiloon.wy@gmail.com>
2023-01-25 13:28:30 +0800

    arm image test

--- deploy/k8s/k8s-rssx-api.sh
+++ deploy/k8s/k8s-rssx-api.sh
@@ -1,16 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
 projectName="rssx-api"
-cd /home/wiloon/projects/rssx/rssx-api || exit
-CGO_ENABLED=1 GOOS=linux GOPROXY=https://goproxy.io go build -v -a -o ${projectName} ${projectName}.go
-ls -l /home/wiloon/projects/rssx/rssx-api/rssx-api
-# build image
-sudo buildah bud -t registry.wiloon.com/rssx-api:v0.0.1 .
-# push to registry
-sudo buildah push registry.wiloon.com/rssx-api:v0.0.1
-echo "y"|sudo podman image prune
-rm /home/wiloon/projects/rssx/rssx-api/rssx-api
+version="v0.0.1"
 
-ansible -i '192.168.50.20,' all -u root -m copy -a 'src=/home/wiloon/workspace/projects/rssx/deploy/k8s/k8s-rssx-api-deployment.yaml dest=/tmp'
-ansible -i '192.168.50.20,' all -u root -m shell -a 'kubectl delete -f /tmp/k8s-rssx-api-deployment.yaml'
-ansible -i '192.168.50.20,' all -u root -m shell -a 'kubectl create -f /tmp/k8s-rssx-api-deployment.yaml'
+cd ~/projects/rssx/rssx-api || exit
+
+# go-sqlite3 requires cgo
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 GOPROXY=https://goproxy.io go build -v -a -o ${projectName} ${projectName}.go
+ls -l ~/projects/rssx/rssx-api/rssx-api
+sudo buildah bud --arch=amd64 -t registry.wiloon.com/rssx-api:${version}-amd64 .
+sudo buildah push registry.wiloon.com/rssx-api:${version}-amd64
+rm ~/projects/rssx/rssx-api/rssx-api
+
+CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc CC_FOR_TARGET=gcc-aarch64-linux-gnu GOPROXY=https://goproxy.io go build -v -a -o ${projectName} ${projectName}.go
+ls -l ~/projects/rssx/rssx-api/rssx-api
+sudo buildah bud --arch=arm64 -t registry.wiloon.com/rssx-api:${version}-arm64 .
+sudo buildah push registry.wiloon.com/rssx-api:${version}-arm64
+rm ~/projects/rssx/rssx-api/rssx-api
+
+podman image rm registry.wiloon.com/rssx-api:v0.0.1
+
+buildah manifest create registry.wiloon.com/rssx-api:${version} \
+    --amend registry.wiloon.com/rssx-api:${version}-amd64 \
+    --amend registry.wiloon.com/rssx-api:${version}-arm64
+
+buildah manifest inspect registry.wiloon.com/rssx-api:${version}
+buildah manifest push --all registry.wiloon.com/rssx-api:${version}  docker://registry.wiloon.com/rssx-api:${version}
+
+echo "y"|sudo podman image prune
+
+ansible -i '192.168.50.228,' all -u root -m copy -a 'src=~/projects/rssx/deploy/k8s/k8s-rssx-api-deployment.yaml dest=/tmp'
+ansible -i '192.168.50.228,' all -u root -m shell -a 'kubectl delete -f /tmp/k8s-rssx-api-deployment.yaml'
+ansible -i '192.168.50.228,' all -u root -m shell -a 'kubectl create -f /tmp/k8s-rssx-api-deployment.yaml'
--- deploy/k8s/k8s-rssx-ui.sh
+++ deploy/k8s/k8s-rssx-ui.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-cd /home/wiloon/projects/rssx/rssx-ui || exit
-yarn install
-yarn build
-sudo buildah bud -t registry.wiloon.com/rssx-ui:v0.0.1 .
-sudo buildah push registry.wiloon.com/rssx-ui:v0.0.1
+version="v0.0.1"
 
-ansible -i '192.168.50.20,' all -u root -m copy -a 'src=/home/wiloon/workspace/projects/rssx/deploy/k8s/k8s-rssx-ui-deployment.yaml dest=/tmp'
-ansible -i '192.168.50.20,' all -u root -m shell -a 'kubectl delete -f /tmp/k8s-rssx-ui-deployment.yaml'
-ansible -i '192.168.50.20,' all -u root -m shell -a 'kubectl create -f /tmp/k8s-rssx-ui-deployment.yaml'
+cd ~/projects/rssx/rssx-ui || exit
+yarn install
+export NODE_OPTIONS=--openssl-legacy-provider
+yarn build
+
+sudo buildah bud --arch=amd64 -t registry.wiloon.com/rssx-ui:${version}-amd64 .
+sudo buildah push registry.wiloon.com/rssx-ui:${version}-amd64
+
+sudo buildah bud --arch=arm64 -t registry.wiloon.com/rssx-ui:${version}-arm64 .
+sudo buildah push registry.wiloon.com/rssx-ui:${version}-arm64
+
+rm -rf ~/projects/rssx/rssx-ui/dist
+
+buildah manifest create registry.wiloon.com/rssx-ui:${version} \
+    --amend registry.wiloon.com/rssx-ui:${version}-amd64 \
+    --amend registry.wiloon.com/rssx-ui:${version}-arm64
+
+buildah manifest inspect registry.wiloon.com/rssx-ui:${version}
+buildah manifest push --all registry.wiloon.com/rssx-ui:${version}  docker://registry.wiloon.com/rssx-ui:${version}
+
+
+ansible -i '192.168.50.228,' all -u root -m copy -a 'src=~/projects/rssx/deploy/k8s/k8s-rssx-ui-deployment.yaml dest=/tmp'
+ansible -i '192.168.50.228,' all -u root -m shell -a 'kubectl delete -f /tmp/k8s-rssx-ui-deployment.yaml'
+ansible -i '192.168.50.228,' all -u root -m shell -a 'kubectl create -f /tmp/k8s-rssx-ui-deployment.yaml'
