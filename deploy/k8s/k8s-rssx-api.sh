806a69b402f9ded3f12a8011d96d1d2f9705ba12
wiloon <wiloon.wy@gmail.com>
2023-01-26 15:33:59 +0800

    arm64 running

--- deploy/k8s/k8s-rssx-api.sh
+++ deploy/k8s/k8s-rssx-api.sh
@@ -1,35 +1,41 @@
 #!/bin/sh
 
-projectName="rssx-api"
+project_name="rssx-api"
 version="v0.0.1"
 
 cd ~/projects/rssx/rssx-api || exit
 
 # go-sqlite3 requires cgo
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 GOPROXY=https://goproxy.io go build -v -a -o ${projectName} ${projectName}.go
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 GOPROXY=https://athens.wiloon.com go build -v -a -o ${projectName} ${projectName}.go
 ls -l ~/projects/rssx/rssx-api/rssx-api
 md5sum ~/projects/rssx/rssx-api/rssx-api
-sudo buildah bud --arch=amd64 -t registry.wiloon.com/rssx-api:${version}-amd64 .
-sudo buildah push registry.wiloon.com/rssx-api:${version}-amd64
+
+# Set the required variables
+export REGISTRY="registry.wiloon.com"
+
+sudo podman image ls
+sudo podman manifest rm registry.wiloon.com/rssx-api:${version}
+sudo podman image ls
+
+# Create a multi-architecture manifest
+export manifest_name=${project_name}-manifest
+sudo buildah manifest create ${manifest_name}:${version}
+
+sudo buildah bud --arch=amd64 -t registry.wiloon.com/rssx-api:${version} --manifest ${manifest_name} .
 rm ~/projects/rssx/rssx-api/rssx-api
 
-CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc CC_FOR_TARGET=gcc-aarch64-linux-gnu GOPROXY=https://goproxy.io go build -v -a -o ${projectName} ${projectName}.go
+CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc CC_FOR_TARGET=gcc-aarch64-linux-gnu GOPROXY=https://athens.wiloon.com go build -v -a -o ${projectName} ${projectName}.go
 ls -l ~/projects/rssx/rssx-api/rssx-api
 md5sum ~/projects/rssx/rssx-api/rssx-api
-sudo buildah bud --arch=arm64 -t registry.wiloon.com/rssx-api:${version}-arm64 .
-sudo buildah push registry.wiloon.com/rssx-api:${version}-arm64
+
+sudo podman image ls
+
+sudo buildah bud --arch=arm64 -t registry.wiloon.com/rssx-api:${version} --manifest ${manifest_name} .
 rm ~/projects/rssx/rssx-api/rssx-api
 
-podman image ls
-podman manifest rm registry.wiloon.com/rssx-api:${version}
-podman image ls
-
-buildah manifest create registry.wiloon.com/rssx-api:${version} \
-    --amend registry.wiloon.com/rssx-api:${version}-amd64 \
-    --amend registry.wiloon.com/rssx-api:${version}-arm64
-
-buildah manifest inspect registry.wiloon.com/rssx-api:${version}
-buildah manifest push --all registry.wiloon.com/rssx-api:${version}  docker://registry.wiloon.com/rssx-api:${version}
+sudo buildah manifest push --all \
+    ${manifest_name} \
+    docker://registry.wiloon.com/rssx-api:${version}
 
 echo "y"|sudo podman image prune
 
